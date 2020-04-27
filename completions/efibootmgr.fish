2f39c46de8a2fb55d035537787552d50f66455e2
Martynas Mickevičius <self@2m.lt>
2020-04-27 09:14:53 +0300

    Initial commit

---
+++ completions/efibootmgr.fish
@@ -0,0 +1,11 @@
+function __fish_ebm_using_command
+  set cmd (commandline -opc)
+  if [ (count $cmd) -gt 1 ]
+    if [ $argv[1] = $cmd[2] ]
+      return 0
+    end
+  end
+  return 1
+end
+
+complete -f -c efibootmgr -n '__fish_ebm_using_command --bootnext' -a "efibootmgr | grep Boot0 | sed 's/* /\t/'"
